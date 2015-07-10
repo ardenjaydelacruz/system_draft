06eecf271ca55e05ab47a838e41ae23a9addbae9
Arden Jay Dela Cruz <arden.developer@gmail.com>
2015-07-10 15:54:45 +0800

    before merging to master

--- application/controllers/ems.php
+++ application/controllers/ems.php
@@ -57,7 +57,57 @@ class Ems extends CI_Controller
     public function employees()
     {
         $this->display_navbar('Employees - MSInc.');
-        $this->ems_model->employees_table();
+
+        $config["base_url"] = base_url() . "employee/employees";
+        $config["total_rows"] = $this->ems_model->total_employees();
+        $config["per_page"] = 15;
+        $config["uri_segment"] = 3;
+        $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = round($choice);
+        $config['full_tag_open'] = '<ul class="pagination zero">';
+        $config['full_tag_close'] = '</ul>';
+        $config['first_link'] = false;
+        $config['last_link'] = false;
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['prev_link'] = '&laquo';
+        $config['prev_tag_open'] = '<li class="prev">';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_link'] = '&raquo';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $this->pagination->initialize($config);
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+        $data['total_employee'] = $this->ems_model->total_employees();
+        $data["record"] = $this->ems_model->fetch_record($config["per_page"], $page);
+        $data["links"] = $this->pagination->create_links();
+
+        $this->load->view("employee/employees_table", $data);
+
+        $this->load->view('components/footer');
+        if ($this->session->userdata('added')) {
+            $this->toast('Successful! Record has been added.', 'success');
+            $this->session->unset_userdata('added');
+        }
+        if ($this->session->userdata('deleted')) {
+            $this->toast('Successful! Record has been deleted.', 'success');
+            $this->session->unset_userdata('deleted');
+        }
+        if ($this->session->userdata('edited')) {
+            $this->toast('Successful! Record has been updated.', 'success');
+            $this->session->unset_userdata('edited');
+        }
+        if ($this->session->userdata('uploaded')) {
+            $this->toast('Successful! Photo has been changed.', 'success');
+            $this->session->unset_userdata('uploaded');
+        }
     }
 
     public function search_employee()
